a615e56635bb951d67f06e95389afd177b713d8e
kheedogg <kheedogg@naver.com>
2025-09-16 18:50:34 +0900

    Clean up: Remove duplicate nestjs-test directory and organize project structure

--- nestjs-board-app/nestjs-test/src/auth/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from './user.repository';
-
-@Injectable()
-export class AuthService {
-    constructor(
-        private userRepository: UserRepository,
-    ) {}
-}
